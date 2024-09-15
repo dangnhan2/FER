20b8072855dcc35400aff07b0e974a2a4c16680a
Your Name <you@example.com>
2024-09-15 09:24:17 +0700

    FourthCommit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import "./App.css";
 
 function App() {
   return (
-    <p>Nhan</p>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>Nhan</h1>
+      </header>
+    </div>
   );
 }
 
